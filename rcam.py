8273958e8bff58d0b8952d5307cb1c874318a765
Anthony Su <anthonymarksu@gmail.com>
2025-04-10 17:35:53 -0700

    initial commit

---
+++ rcam.py
@@ -0,0 +1,202 @@
+import numpy as np
+from numpy import deg2rad
+from scipy.constants import g
+from scipy.spatial.transform import Rotation
+
+
+def dcm(phi, theta, psi):
+    return Rotation.from_euler("ZYX", [psi, theta, phi]).as_matrix()
+
+
+def RCAM_model(x, u):
+    """
+    RCAM aircraft model.
+
+    Args:
+        x (numpy.ndarray): State vector.
+                           If length is 9: [u, v, w, p, q, r, phi, theta, psi]
+                           If length is 12: [u, v, w, p, q, r, phi, theta, psi, PN, PE, PD]
+        u (numpy.ndarray): Control vector [d_A, d_T, d_R, d_th1, d_th2]
+
+    Returns:
+        numpy.ndarray: Time derivative of the state vector xDot.
+
+    All inputs are in SI base units. Angles are in radians.
+    """
+    # Constants
+    rho = 1.225  # air density at sea level
+    n = 5.5  # lift curve slope
+    alpha_L0 = deg2rad(-11.5)  # zero lift AOA
+    a0 = 15.212  # wing body stall regime model
+    a1 = -155.2  # \
+    a2 = 609.2  #  } stall model coefficients
+    a3 = -768.5  # /
+    l_t = 24.8  # tail distance
+    depsilondalpha = 0.25  # downwash model
+    S = 260  # wing area
+    S_t = 64  # (horizontal) tail area
+    c_bar = 6.6  # mean aerodynamic chord
+    m = 120000  # mass
+    r_E1b = np.array([0, -7.94, -1.9])  # engine 1 position in body frame
+    r_E2b = np.array([0, 7.94, -1.9])  # engine 2 position in body frame
+    I_b = m * np.array(
+        [[40.07, 0, -2.0923], [0, 64, 0], [-2.0923, 0, 99.92]]
+    )  # rotational inertia
+
+    # Intermediate Variables
+    V_A = np.sqrt(x[0] ** 2 + x[1] ** 2 + x[2] ** 2)  # airspeed
+    alpha = np.arctan2(x[2], x[0])  # AOA
+    beta = np.arcsin(x[1] / V_A)  # sideslip angle
+    q = 0.5 * rho * V_A**2  # dynamic pressure
+    omega_beb = np.array(
+        [x[3], x[4], x[5]]
+    )  # rotation w.r.t. earth in body frame
+    V_b = np.array([x[0], x[1], x[2]])  # velocity in body frame
+
+    ## CONTROL SATURATION LIMITS -----------------------------------------------
+    u_min = deg2rad(np.array([-25, -25, -30, 0.5, 0.5]))
+    u_max = deg2rad(np.array([25, 10, 30, 1, 1]))
+    u[u < u_min] = u_min[u < u_min]
+    u[u > u_max] = u_max[u > u_max]
+
+    ## AERODYNAMIC FORCES ------------------------------------------------------
+
+    # Wing-Body Lift Coefficient in Stability Frame
+    if alpha <= deg2rad(14.5):
+        C_Lwb = n * (alpha - alpha_L0)  # linear regime
+    else:
+        C_Lwb = a3 * alpha**3 + a2 * alpha**2 + a1 * alpha + a0  # stall regime
+
+    # Tail Lift Coefficient in Stability Frame
+    epsilon = depsilondalpha * (alpha - alpha_L0)  # downwash
+    alpha_t = alpha - epsilon + u[1] + 1.3 * x[4] * l_t / V_A  # tail AOA
+    C_Lt = 3.1 * S_t / S * alpha_t
+
+    # Total Lift Coefficient in Stability Frame
+    C_L = C_Lwb + C_Lt
+
+    # Drag Coefficient in Stability Frame
+    C_D = 0.13 + 0.07 * (5.5 * alpha + 0.654) ** 2
+
+    # Side Force Coeffficient in Stability Frame
+    C_Y = -1.6 * beta + 0.24 * u[2]
+
+    # Aerodynamic Forces in Stability Frame
+    F_As = np.array([-C_D * q * S, C_Y * q * S, -C_L * q * S])
+
+    # Aerodynamic Forces in Body Frame
+    C_bs = np.array(
+        [
+            [np.cos(alpha), 0, -np.sin(alpha)],
+            [0, 1, 0],
+            [np.sin(alpha), 0, np.cos(alpha)],
+        ]
+    )  # rotation matrix
+    F_Ab = C_bs @ F_As
+
+    ## AERODYNAMIC MOMENTS -----------------------------------------------------
+
+    # Aerodynamic Moment Coefficients about AC in Body Frame
+    eta_bar = np.array(
+        [
+            -1.4 * beta,
+            -0.59 - 3.1 * S_t * l_t / (S * c_bar) * (alpha - epsilon),
+            (1 - alpha * 180 / (15 * np.pi)) * beta,
+        ]
+    )  # orientation effects
+    dcmdx = np.array(
+        [
+            [-11, 0, 5],
+            [0, -4.03 * S_t * (l_t**2) / (S * (c_bar**2)), 0],
+            [1.7, 0, -11.5],
+        ]
+    ) * (
+        c_bar / V_A
+    )  # rotation rate sensitivity
+    dcmdu = np.array(
+        [
+            [-0.6, 0, 0.22],
+            [0, -3.1 * S_t * l_t / (S * c_bar), 0],
+            [0, 0, -0.63],
+        ]
+    )  # control sensitivity
+    C_Macb = eta_bar + dcmdx @ x[3:6] + dcmdu @ u[:3]
+
+    # Aerodynamic Moments about AC in Body Frame
+    M_Aacb = C_Macb * q * S * c_bar
+
+    # Aerodynamic Moments about CG in Body Frame
+    r_cgb = np.array(
+        [0.23 * c_bar, 0, 0.10 * c_bar]
+    )  # CG position in body frame
+    r_acb = np.array([0.12 * c_bar, 0, 0])  # AC position in body frame
+    M_Acgb = M_Aacb + np.cross(F_Ab, (r_cgb - r_acb))
+
+    ## ENGINE FORCES AND MOMENTS -----------------------------------------------
+
+    # Engine Forces in Body Frame
+    F_E1b = np.array([u[3] * m * g, 0, 0])  # engine 1 force
+    F_E2b = np.array([u[4] * m * g, 0, 0])  # engine 2 force
+    F_Eb = F_E1b + F_E2b
+
+    # Engine Moments about CG in Body Frame
+    mu_bar1 = np.array(
+        [r_cgb[0] - r_E1b[0], r_E1b[1] - r_cgb[1], r_cgb[2] - r_E1b[2]]
+    )  # engine 1 moment arm
+    mu_bar2 = np.array(
+        [r_cgb[0] - r_E2b[0], r_E2b[1] - r_cgb[1], r_cgb[2] - r_E2b[2]]
+    )  # engine 2 moment arm
+    M_E1cgb = np.cross(mu_bar1, F_E1b)  # engine 1 moment
+    M_E2cgb = np.cross(mu_bar2, F_E2b)  # engine 2 moment
+    M_Ecgb = M_E1cgb + M_E2cgb
+
+    ## GRAVITY FORCES
+    F_gb = (
+        np.array(
+            [
+                -np.sin(x[7]),
+                np.cos(x[7]) * np.sin(x[6]),
+                np.cos(x[7]) * np.cos(x[6]),
+            ]
+        )
+        * m
+        * g
+    )
+
+    ## FIRST-ORDER EXPLICIT SYSTEM ---------------------------------------------
+
+    xDot = np.zeros(len(x))
+
+    # Linear Acceleration
+    F_b = F_gb + F_Eb + F_Ab  # total forces in body frame
+    xDot[0:3] = (1 / m) * F_b - np.cross(omega_beb, V_b)
+
+    # Rotational Acceleration
+    M_cgb = M_Ecgb + M_Acgb  # total moments in body frame
+    xDot[3:6] = np.linalg.solve(
+        I_b, (M_cgb - np.cross(omega_beb, (I_b @ omega_beb)))
+    )
+
+    # Euler Rates
+    phi = x[6]
+    theta = x[7]
+    ERmat = np.array(
+        [
+            [1, np.sin(phi) * np.tan(theta), np.cos(phi) * np.tan(theta)],
+            [0, np.cos(phi), -np.sin(phi)],
+            [0, np.sin(phi) / np.cos(theta), np.cos(phi) / np.cos(theta)],
+        ]
+    )  # Euler rate from body rate transform
+    xDot[6:9] = ERmat @ x[3:6]
+
+    ## APPEND NORTH-EAST-DOWN POSITION RATES -----------------------------------
+
+    if len(x) == 12:
+        # DCM From Body to NED Frame
+        phi, theta, psi = x[6:9]
+        C_vb = dcm(phi, theta, psi)
+
+        # Transform u,v,w to V_NED
+        xDot[9:12] = C_vb @ x[0:3]
+
+    return xDot
