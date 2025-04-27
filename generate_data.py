1382b2d50fcaf797b15f3b5cde37075fb554954e
Billy Tubbs <wrtubbs3@gmail.com>
2025-04-26 20:24:07 -0700

    Edited approach to saving data file (.npy instead of .npz)

--- generate_data.py
+++ generate_data.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from compute_trajectory import simulate_trajectory
 from rcam import rcam_system, rcam_plant
 from common_plotting import *
+import time
 
 # ==== LOAD PREVIOUSLY SAVED CONTROL PROFILES AND TRIM POINTS ====
 
@@ -28,11 +29,13 @@ saturation_limits = {
 
 RCAM_data = {}
 
-# Dummy index to limit iterations for testing
-dummy_idx = 0
+# Start timer
+start_time = time.time()
 
 for trim_case, trim_data in trim_results.items():
     RCAM_data[trim_case] = {}
+
+    print(f"Running simulation for Trim Case: {trim_case}")
 
     # Extract trim data
     x0 = trim_data['x_trim']
@@ -40,75 +43,74 @@ for trim_case, trim_data in trim_results.items():
 
     for profile_ID, profile_data in control_profiles.items():
         
-        # if dummy_idx == 4 or dummy_idx == 8 or dummy_idx == 12 or dummy_idx == 16 or dummy_idx == 20 or dummy_idx == 24:
-        if dummy_idx == 28 or dummy_idx == 32:
-            print(f"Running simulation for Trim Case: {trim_case}, Control Profile: {profile_ID}")
+        # print(f"Running simulation for Trim Case: {trim_case}, Control Profile: {profile_ID}")
 
-            # Extract control profile
-            t_vec = profile_data[:, 0]      # time vector
-            u_profile = profile_data[:, 1:]     # control profile time history
+        # Extract control profile
+        t_vec = profile_data[:, 0]      # time vector
+        u_profile = profile_data[:, 1:]     # control profile time history
 
-            # Build control input array and apply saturation limits
-            u_unlimited = u_profile + u0    # Add trim control inputs to profile
-            mins = np.array([limit[0] for limit in saturation_limits.values()])     # Build array of min. saturation limits
-            maxs = np.array([limit[1] for limit in saturation_limits.values()])     # Build array of max. saturation limits
-            u = np.clip(u_unlimited, mins, maxs)
+        # Build control input array and apply saturation limits
+        u_unlimited = u_profile + u0    # Add trim control inputs to profile
+        mins = np.array([limit[0] for limit in saturation_limits.values()])     # Build array of min. saturation limits
+        maxs = np.array([limit[1] for limit in saturation_limits.values()])     # Build array of max. saturation limits
+        u = np.clip(u_unlimited, mins, maxs)
 
-            # # Simulate the RCAM aircraft model
-            # x0_full = np.concatenate((x0, np.zeros(9)))
-            # t_out, x_out = simulate_trajectory(
-            #     f=rcam_system, x0=x0_full, t_in=t_vec, u_in=u, t_out=t_vec
-            # )
+        # # Simulate the RCAM aircraft model
+        # x0_full = np.concatenate((x0, np.zeros(9)))
+        # t_out, x_out = simulate_trajectory(
+        #     f=rcam_system, x0=x0_full, t_in=t_vec, u_in=u, t_out=t_vec
+        # )
 
-            # Simulate the RCAM plant model
-            t_out, x_out = simulate_trajectory(
-                f=rcam_plant, x0=x0, t_in=t_vec, u_in=u, t_out=t_vec
-            )
+        # Simulate the RCAM plant model
+        t_out, x_out = simulate_trajectory(
+            f=rcam_plant, x0=x0, t_in=t_vec, u_in=u, t_out=t_vec
+        )
 
-            # Store results
-            RCAM_data[trim_case][profile_ID] = {
-                "time": t_vec,
-                "u": u, 
-                "x": x_out
-            }
-
-        dummy_idx += 1
+        # Store results
+        RCAM_data[trim_case][profile_ID] = {
+            "time": t_vec,
+            "u": u, 
+            "x": x_out
+        }
 
 # print(f"RCAM_data: {RCAM_data}")
 
-# Plot response for each trim case and control profile
-for trim_case, profile_data in RCAM_data.items():
-    for profile_ID, data in profile_data.items():
-        t = data["time"]
-        x = data["x"]
-        u = data["u"]
+# End timer
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Simulations completed in {elapsed_time:.2f} seconds (total)")
 
-        # Plot state variables
-        plot_state_variables(t, x, f"State Variables for {trim_case} with {profile_ID}")
+# # Plot response for each trim case and control profile
+# for trim_case, profile_data in RCAM_data.items():
+#     for profile_ID, data in profile_data.items():
+#         t = data["time"]
+#         x = data["x"]
+#         u = data["u"]
 
-        # Plot trajectory
-        plot_trajectory(t, x, f"Trajectory for {trim_case} with {profile_ID}")
+#         # Plot state variables
+#         plot_state_variables(t, x, f"State Variables for {trim_case} with {profile_ID}")
 
-        # Plot roll axis trajectories
-        plot_roll(t, x, u, f"Roll Axis Trajectories for {trim_case} with {profile_ID}")
+#         # Plot trajectory
+#         plot_trajectory(t, x, f"Trajectory for {trim_case} with {profile_ID}")
 
-        # Plot pitch axis & throttle trajectories
-        plot_pitch_throttle(t, x, u, f"Pitch Axis & Throttle Trajectories for {trim_case} with {profile_ID}")
+#         # Plot roll axis trajectories
+#         plot_roll(t, x, u, f"Roll Axis Trajectories for {trim_case} with {profile_ID}")
 
-        # PLot yaw axis trajectories
-        plot_yaw(t, x, u, f"Yaw Axis Trajectories for {trim_case} with {profile_ID}")
+#         # Plot pitch axis & throttle trajectories
+#         plot_pitch_throttle(t, x, u, f"Pitch Axis & Throttle Trajectories for {trim_case} with {profile_ID}")
+
+#         # PLot yaw axis trajectories
+#         plot_yaw(t, x, u, f"Yaw Axis Trajectories for {trim_case} with {profile_ID}")
 
 # ==== SAVE RESULTS TO FILE ====
 
-# # Save all results to one file
-# np.savez("RCAM_data.npz", **RCAM_data)
+# Save all results to one file
+output_file_path = "RCAM_data.npy"
+np.save(output_file_path, RCAM_data)
+print(f"Saved output data as {output_file_path}")
 
 # ==== EXAMPLE FILE LOADING ====
 
 # # Load the results
+# RCAM_data = np.load("RCAM_data.npy", allow_pickle=True).item()
 # loaded = np.load("simulation_results.npz", allow_pickle=True)
-# results = loaded["results"].item()
-
-# # Example access
-# time = results['trim1']['step_input']['time']
-# states = results['trim1']['step_input']['states']
